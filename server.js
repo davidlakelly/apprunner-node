969bcd321a214f798c8fa10f77f6f472a195c319
David <davidlakelly@gmail.com>
2024-07-22 21:41:04 +0100

    Init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set the static files directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
